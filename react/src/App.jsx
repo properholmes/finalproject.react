43959f46744844e49cb7bb4d883443529b00b745
wats0nUnMEduB13 <nghtrsnl@gmail.com>
2024-08-30 16:43:34 -0400

    we in here

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  //we need to return something here
+  const name = "Chris"
+
+  return (
+    <>
+      <h3>Hello {name}!</h3>
+    </>
+  )
+  
+}
+
+export default App
